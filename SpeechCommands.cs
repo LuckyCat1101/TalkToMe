b3b009fa5731fb7457a789950c5167f701d30576
Vanesa <vpere159@fiu.edu>
2018-10-02 12:26:22 -0400

    Speech Commands

---
+++ SpeechCommands.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Speech_Recognition
+{
+    public partial class SpeechToTextForm : Form
+    {
+        SpeechRecognitionEngine recognitionEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        public SpeechToTextForm()
+        {
+            InitializeComponent();
+        }
+
+        private void enableVoiceBttn_Click(object sender, EventArgs e)
+        {
+            recognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            disableVoiceBttn.Enabled = true;
+        }
+
+        private void SpeechToTextForm_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "say hello", "print my name", "speak selected text" });
+            GrammarBuilder grammarBuilder = new GrammarBuilder();
+            grammarBuilder.Append(commands);
+            Grammar grammar = new Grammar(grammarBuilder);
+
+            recognitionEngine.LoadGrammarAsync(grammar);
+            recognitionEngine.SetInputToDefaultAudioDevice();
+            recognitionEngine.SpeechRecognized += RecognitionEngine_SpeechRecognized;
+        }
+
+        private void RecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "say hello":
+                    PromptBuilder builder = new PromptBuilder();
+
+                    builder.StartSentence();
+                    builder.AppendText("Hello Vanesa!");
+                    builder.EndSentence();
+
+                    builder.StartSentence();
+                    builder.AppendText("How are you?", PromptEmphasis.Moderate);
+                    builder.EndSentence();
+
+                    synthesizer.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Teen);
+                    synthesizer.SpeakAsync(builder);
+                    break;
+                case "print my name":
+                    richTextBox.Text += "\nVanesa";
+                    break;
+                case "speak selected text":
+                    synthesizer.SpeakAsync(richTextBox.SelectedText);
+                    break;
+
+            }
+        }
+
+        private void disableVoiceBttn_Click(object sender, EventArgs e)
+        {
+            recognitionEngine.RecognizeAsyncStop();
+            disableVoiceBttn.Enabled = false;
+        }
+    }
+}
